6cc84490e62ca30ad4c5f52dedc945458a4eed2d
DESKTOP-HAORFNM\Mahdi <mahdi.ghorbani@gmail.com>
2021-08-09 00:11:18 +0430

    add authorization to the gateway.

--- misc/Stitching/federated-with-pull/gateway/Startup.cs
+++ misc/Stitching/federated-with-pull/gateway/Startup.cs
@@ -1,12 +1,38 @@
 using System;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
 using HotChocolate.AspNetCore;
+using HotChocolate.Execution;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace Demo.Gateway
 {
+    public class HttpRequestInterceptor : DefaultHttpRequestInterceptor
+    {
+        public override ValueTask OnCreateAsync(HttpContext context,
+            IRequestExecutor requestExecutor, IQueryRequestBuilder requestBuilder,
+            CancellationToken cancellationToken)
+        {
+            //impersonate the user for athorization test
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Role, "Administrator"),
+                new Claim(ClaimTypes.NameIdentifier, "Test"),
+                new Claim(ClaimTypes.Name, "User1")
+            }, "someAuthTypeName"));
+
+
+            context.User = user;
+
+            return base.OnCreateAsync(context, requestExecutor, requestBuilder,
+                cancellationToken);
+        }
+    }
     public class Startup
     {
         public const string Accounts = "accounts";
@@ -18,22 +44,31 @@ namespace Demo.Gateway
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddHttpClient(Accounts, c => c.BaseAddress = new Uri("http://localhost:5051/graphql"));
+            services.AddHttpClient(Accounts, c => c.BaseAddress = new Uri("http://localhost:5056/graphql"));
             services.AddHttpClient(Inventory, c => c.BaseAddress = new Uri("http://localhost:5052/graphql"));
             services.AddHttpClient(Products, c => c.BaseAddress = new Uri("http://localhost:5053/graphql"));
             services.AddHttpClient(Reviews, c => c.BaseAddress = new Uri("http://localhost:5054/graphql"));
 
+            services.AddAuthorization(options =>
+            {
+
+                options.AddPolicy("HasCountry", policy =>
+                    policy.RequireAssertion(context =>
+                        context.User.HasClaim(c => c.Type == ClaimTypes.Country)));
+            });
             services
                 .AddGraphQLServer()
                 .AddQueryType(d => d.Name("Query"))
                 .AddRemoteSchema(Accounts)
-                .AddRemoteSchema(Inventory)
-                .AddRemoteSchema(Products)
-                .AddRemoteSchema(Reviews);
-        }
+			    .AddRemoteSchema(Inventory)
+				.AddRemoteSchema(Products)
+				.AddRemoteSchema(Reviews)
+			    .AddAuthorization()
+                .AddHttpRequestInterceptor<HttpRequestInterceptor>();
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -41,7 +76,8 @@ namespace Demo.Gateway
             }
 
             app.UseRouting();
-
+            app.UseAuthentication();
+            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGraphQL();
